5d16bae19fcc3fa50bcf8068c0586e3e69a0c4db
salvatoluice <salvatoluice5@gmail.com>
2023-05-29 23:34:18 +0300

    Default imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <Navbar />
+      <Footer />
+    </>
   )
 }
 
